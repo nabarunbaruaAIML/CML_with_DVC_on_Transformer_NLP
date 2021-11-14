0d071cbec5f5ba17f7a4cf5166ad3d368837d42b
Nabarun Barua <64695833+nabarunbaruaAIML@users.noreply.github.com>
2021-11-14 13:10:42 +0530

    Initial Changes Stage 1

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+STAGE = 'Template'
+def main(config_path):
+    pass
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info(f">>>>> {STAGE} started <<<<<")
+        main(parsed_args)
+        logging.info(f">>>>> {STAGE}  completed! <<<<<\n\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
