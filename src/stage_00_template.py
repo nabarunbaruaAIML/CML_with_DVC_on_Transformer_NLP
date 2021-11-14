afffbabd3d6e40eb29a251343c76a155836ff311
Nabarun Barua <64695833+nabarunbaruaAIML@users.noreply.github.com>
2021-11-14 16:29:55 +0530

    Minnor Template

--- src/stage_00_template.py
+++ src/stage_00_template.py
@@ -4,7 +4,10 @@ import shutil
 from tqdm import tqdm
 import logging
 
-
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+# create_directory([log_dir])
+os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
     level=logging.INFO, 
@@ -13,12 +16,16 @@ logging.basicConfig(
     )
 STAGE = 'Template'
 def main(config_path):
+    # config = read_yaml(config_path.config)
+    # params = read_yaml(config_path.params)
+    # secret = read_yaml(config_path.secret)
     pass
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
+    args.add_argument("--secret","-s", default="configs/secrets.yaml")
     parsed_args = args.parse_args()
 
     try:
