476bfbb29e20523a0fc903fbc55984a391877c1d
Nabarun Barua <64695833+nabarunbaruaAIML@users.noreply.github.com>
2021-12-12 15:36:04 +0530

    Stage 04 Initial Commit

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -3,6 +3,8 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
+from datasets import load_from_disk
+from src.utils.all_utils import read_yaml
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -16,16 +18,18 @@ logging.basicConfig(
     )
 STAGE = 'Template'
 def main(config_path):
-    # config = read_yaml(config_path.config)
-    # params = read_yaml(config_path.params)
+    # dataset = load_from_disk('artifacts/Data/Dataset')
+    # print(dataset)
+    config = read_yaml(config_path.config)
+    params = read_yaml(config_path.params)
     # secret = read_yaml(config_path.secret)
-    pass
+    # pass
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
-    args.add_argument("--secret","-s", default="configs/secrets.yaml")
+    # args.add_argument("--secret","-s", default="configs/secrets.yaml")
     parsed_args = args.parse_args()
 
     try:
